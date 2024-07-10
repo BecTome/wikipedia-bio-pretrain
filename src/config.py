d5f2b7fdc07d52ba3074872bdf8e34f3ea34f92d
Alberto Becerra <abecerr1@bsc.es>
2024-07-10 19:32:48 +0200

    Basic settings

--- src/config.py
+++ src/config.py
@@ -1,7 +1,10 @@
-D_LANG_CAT = {"es": "Category:Ciencias de la vida",
-              "en": "Category:Life Sciences"}
+D_LANG_CAT = {"es": "Categoría:Ciencias de la vida",
+              "en": "Category:Life_Sciences"}
 
 AGENT = "MyProjectName (merlin@example.com)"
 
-MAX_DEPTH = 5
+MAX_DEPTH = 4
 MIN_DEPTH = 0
+
+LOG_FOLDER = "logs"
+OUTPUT_FOLDER = "output"
